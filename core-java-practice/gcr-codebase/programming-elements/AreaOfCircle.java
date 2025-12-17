46ba9096c3271577aa8c8e8909a260d4a50c79b5
Sanju <psinghrajput907@gmail.com>
2025-12-17 15:32:04 +0530

    [ Pushpendra Singh ] Solved 4 basic question in gcr codebase

---
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double area=3.14*r*r;
+        System.out.println("Area of circle is: "+area);
+    }
+}
