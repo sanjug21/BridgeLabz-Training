a9973a709f0b6a40b70d0aabfaec2a20e8116a07
Sanju <psinghrajput907@gmail.com>
2025-12-17 14:42:02 +0530

    [ Pushpendra Singh ] Solved the five basic questions

---
+++ core-java-practice/leet-code-codebase/palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class palindrome{
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string to check if it's a palindrome:");
+        String s = sc.nextLine();
+
+        int n=s.length();
+        int i=0, j=n-1;
+        boolean isPalindrome = true;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                isPalindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        System.out.println("Is the string a palindrome? " + isPalindrome);
+     }
+}
