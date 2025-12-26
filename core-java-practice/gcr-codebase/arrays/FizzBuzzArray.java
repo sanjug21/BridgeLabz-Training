09b8459df271ce096380ae6ffa82ff1a1b94f6df
Sanju <psinghrajput907@gmail.com>
2025-12-26 15:13:48 +0530

    [ Pushpendra Singh ] Refactored the names of files to avoid errors

--- core-java-practice/gcr-codebase/arrays/FizzBuzzArray.java
+++ core-java-practice/gcr-codebase/arrays/FizzBuzzArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FizzBuzz {
+public class FizzBuzzArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a positive integer: ");
--- core-java-practice/gcr-codebase/arrays/FriendsComparisonArray.java
+++ core-java-practice/gcr-codebase/arrays/FriendsComparisonArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FriendsComparison {
+public class FriendsComparisonArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int[] age=new int[3];
