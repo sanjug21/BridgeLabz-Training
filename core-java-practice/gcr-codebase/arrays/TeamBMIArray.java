b34870ba0bafff41d324faf5db7ec63ba97015e5
Sanju <you@example.com>
2025-12-26 15:04:56 +0530

    [ Pushpendra Singh ] Refactored the names of files to avoid errors

--- core-java-practice/gcr-codebase/arrays/TeamBMIArray.java
+++ core-java-practice/gcr-codebase/arrays/TeamBMIArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TeamBMI {
+public class TeamBMIArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter number of persons: ");
