1df5a18cbe28fa44f1ad7ffc9bbda8f34095c770
Sanju <psinghrajput907@gmail.com>
2025-12-26 15:18:06 +0530

    [ Pushpendra Singh ] Refactored the names of files to avoid errors

--- core-java-practice/gcr-codebase/programming-elements/QuotientRemainderBasic.java
+++ core-java-practice/gcr-codebase/programming-elements/QuotientRemainderBasic.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class QuotientRemainder {
+public class QuotientRemainderBasic {
+        
         public static void main(String[] args) {
                 Scanner sc=new Scanner(System.in);
                 int dividend=sc.nextInt();
