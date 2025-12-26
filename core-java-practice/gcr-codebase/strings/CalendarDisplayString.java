e64118ebb217c8c123d7d849242ee98bbadb870f
Sanju <you@example.com>
2025-12-26 15:22:27 +0530

    [ Pushpendra Singh ] Refactored the names of files to avoid errors

--- core-java-practice/gcr-codebase/strings/CalendarDisplayString.java
+++ core-java-practice/gcr-codebase/strings/CalendarDisplayString.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class CalendarDisplay {
-
+public class CalendarDisplayString {
+    
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter month (1-12): ");
