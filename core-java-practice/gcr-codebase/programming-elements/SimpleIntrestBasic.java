0089a57ac24d6cf58349a805941a4f66b7d8e23a
Sanju <you@example.com>
2025-12-26 15:18:06 +0530

    [ Pushpendra Singh ] Refactored the names of files to avoid errors

--- core-java-practice/gcr-codebase/programming-elements/SimpleIntrestBasic.java
+++ core-java-practice/gcr-codebase/programming-elements/SimpleIntrestBasic.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SimpleIntrest {
+public class SimpleIntrestBasic {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int principle=sc.nextInt();
