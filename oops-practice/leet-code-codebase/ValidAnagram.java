350241aa884035c76d37372244ee10107c375c24
Sanju <psinghrajput907@gmail.com>
2025-12-30 12:18:21 +0530

    [ Pushpendra Singh ] Solved some leetcode questions based on array and a graph problem

---
+++ oops-practice/leet-code-codebase/ValidAnagram.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ValidAnagram {
+    // 242. Valid Anagram
+    // https://leetcode.com/problems/valid-anagram/
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first string:");
+        String s=sc.next();
+        System.out.println("Enter second string:");
+        String t=sc.next();
+
+        ValidAnagram obj=new ValidAnagram();
+        boolean ans = obj.isAnagram(s, t);
+        System.out.println("Is Anagram: " + ans);
+        sc.close();
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        int[] count=new int[26];
+        for(char c:s.toCharArray())count[c-'a']++;
+        for(char c:t.toCharArray())count[c-'a']--;
+        for(int i:count)if(i!=0)return false;
+        return true;
+    }
+}
