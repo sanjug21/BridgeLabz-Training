ebb553f4c5cc3422763e680bd75e163b685c9d81
Sanju <psinghrajput907@gmail.com>
2026-01-09 11:45:37 +0530

    [ Pushpendra Singh ] Add. Middle of Linked List and revese the linked list

---
+++ dsa-practice/leet-code-codebase/MiddleOfLinkedList.java
@@ -0,0 +1,34 @@
+public class MiddleOfLinkedList {
+    // 876. Middle of the Linked List
+    // https://leetcode.com/problems/middle-of-the-linked-list/
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static void main(String[] args) {
+        ListNode head=new ListNode(1);
+        head.next=new ListNode(2);
+        head.next.next=new ListNode(3);
+        head.next.next.next=new ListNode(4);
+        head.next.next.next.next=new ListNode(5);
+
+        MiddleOfLinkedList obj=new MiddleOfLinkedList();
+        ListNode ans = obj.middleNode(head);
+        System.out.println("Middle Node: " + ans.val);
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
