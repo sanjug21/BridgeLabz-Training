132cb99ae8bd516ff5b51a1531354ba74ae20ae3
Pushpendra Singh <122636706+sanjug21@users.noreply.github.com>
2026-01-20 15:57:53 +0530

    Delete collections/gcr-codebase/generics directory

--- collections/gcr-codebase/generics/resume_screening_system/ProductManager.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public class ProductManager extends JobRole {
-    public ProductManager() {
-        super("Product Manager", List.of("Agile", "Roadmap", "User Stories", "Communication"));
-    }
-}
