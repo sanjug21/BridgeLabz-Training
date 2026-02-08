90a15ae354b92db619728033c80635686cbfa707
Sanju <psinghrajput907@gmail.com>
2026-02-08 17:44:20 +0530

    [ Pushpendra Singh ] add. libraray management system

---
+++ solution-designing-practice/gcr-codebase/library_management_system/UserFactory.java
@@ -0,0 +1,17 @@
+class UserFactory {
+    public static User createUser(String type, String name) {
+        if (type == null) {
+            throw new IllegalArgumentException("Unknown type");
+        }
+        switch (type.toLowerCase()) {
+            case "student":
+                return new Student(name);
+            case "faculty":
+                return new Faculty(name);
+            case "librarian":
+                return new Librarian(name);
+            default:
+                throw new IllegalArgumentException("Unknown type");
+        }
+    }
+}
